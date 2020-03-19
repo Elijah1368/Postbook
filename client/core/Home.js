048a722e4c77214a68154ca741fc80bfdedf638b
Elijah Amian <eramian125@gmail.com>
2020-03-19 10:37:28 -1000

    Add Routing

--- client/core/Home.js
+++ client/core/Home.js
@@ -3,9 +3,18 @@ import PropTypes from 'prop-types'
 import {withStyles} from 'material-ui/styles'
 import Card, {CardContent, CardMedia} from 'material-ui/Card'
 import Typography from 'material-ui/Typography'
-import sceneImg from './../assets/images/scene.png'
+import seashellImg from './../assets/images/seashell.jpg'
+import {Link} from 'react-router-dom'
+import Grid from 'material-ui/Grid'
+import auth from './../auth/auth-helper'
+import FindPeople from './../user/FindPeople'
+import Newsfeed from './../post/Newsfeed'
 
 const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    margin: 30,
+  },
   card: {
     maxWidth: 600,
     margin: 'auto',
@@ -21,15 +30,54 @@ const styles = theme => ({
 })
 
 class Home extends Component {
+  state = {
+    defaultPage: true
+  }
+  init = () => {
+    if(auth.isAuthenticated()){
+      this.setState({defaultPage: false})
+    }else{
+      this.setState({defaultPage: true})
+    }
+  }
+  componentWillReceiveProps = () => {
+    this.init()
+  }
+  componentDidMount = () => {
+    this.init()
+  }
   render() {
     const {classes} = this.props
     return (
-        <Card className={classes.card}>
-          <Typography type="headline" component="h2" className={classes.title}>
-            Home Page
-          </Typography>
-          <CardMedia className={classes.media} image={sceneImg} title="Unicorn Shells"/>
-        </Card>
+      <div className={classes.root}>
+        {this.state.defaultPage &&
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <Card className={classes.card}>
+                <Typography type="headline" component="h2" className={classes.title}>
+                  Home Page
+                </Typography>
+                <CardMedia className={classes.media} image={seashellImg} title="Unicorn Shells"/>
+                <CardContent>
+                  <Typography type="body1" component="p">
+                    Welcome to the MERN Social home page. 
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        }
+        {!this.state.defaultPage &&
+          <Grid container spacing={24}>
+            <Grid item xs={8} sm={7}>
+              <Newsfeed/>
+            </Grid>
+            <Grid item xs={6} sm={5}>
+              <FindPeople/>
+            </Grid>
+          </Grid>
+        }
+      </div>
     )
   }
 }
